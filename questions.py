18e7356b1d83a8daa9a908755ad0685a7c4844ef
idanison <idanison@hedcontrols.com>
2021-10-21 13:44:54 -0500

    asdf

--- questions.py
+++ questions.py
@@ -1,58 +1,58 @@
 questions = []
 
 question1="Which of the following is delivered at the end of a sprint?"
-question1options="a) A document containing test cases for the current sprint/n b) An architectural design of the solution /n c) An increment of Done software /n d) Wireframes designs for User Interface /n e) A signed statement of work"
+question1options="a) A document containing test cases for the current sprint\n b) An architectural design of the solution \n c) An increment of Done software \n d) Wireframes designs for User Interface \n e) A signed statement of work"
 question1answer="C"
 
 questions.append((question1, question1options, question1answer))
 
 question2="Product Backlog should be ordered on the basis of?"
-question2options="a) Value of the items being delivered /n b) The complexity of the items being delivered /n c) Size of the items being delivered /n d) The risk associated with the items /n e) Based on the Scrum Team choice"
+question2options="a) Value of the items being delivered \n b) The complexity of the items being delivered \n c) Size of the items being delivered \n d) The risk associated with the items \n e) Based on the Scrum Team choice"
 question2answer= "A"
 
 questions.append((question2, question2options, question2answer))
 
 question3 = "In an Agile environment, what is the main responsibility of a tester?"
-question3options = "a) Create test scenarios and test cases /n b) Finding bugs /n c) Create automation scripts /n d) Send test execution reports to the stakeholders /n e) There is no role as a Tester in Scrum"
+question3options = "a) Create test scenarios and test cases \n b) Finding bugs \n c) Create automation scripts \n d) Send test execution reports to the stakeholders \n e) There is no role as a Tester in Scrum"
 question3answer = "E"
 questions.append((question3, question3options, question3answer))
 
 question4 = "When is a Sprint Retrospective ceremony performed?"
-question4options = "a) Whenever the team suggests /n b) At the end of each Sprint /n c) Whenever needed /n d) Whenever the Product Owner suggests /n e) Whenever the Scrum Master suggests"
+question4options = "a) Whenever the team suggests \n b) At the end of each Sprint \n c) Whenever needed \n d) Whenever the Product Owner suggests \n e) Whenever the Scrum Master suggests"
 question4answer = "B"
 questions.append((question4, question4options, question4answer))
 
 question5 = "When can a Sprint be canceled?"
-question5options = "a) The Sprint items are no longer needed /n b) Sprint can never be canceled /n c) When Development is unable to complete the work /n d) Information required to start the development is not available /n e) Whenever the Product Owner says"
+question5options = "a) The Sprint items are no longer needed \n b) Sprint can never be canceled \n c) When Development is unable to complete the work \n d) Information required to start the development is not available \n e) Whenever the Product Owner says"
 question5answer = "E"
 questions.append((question5, question5options, question5answer))
 
 question6 = "What is a Sprint Review?"
-question6options = "a) Activity to Introspect and Adapt /n b) Activity to improve Scrum Processes /n c) Activity to seek approval for the work done /n d) Activity to plan for the next Sprint /n e) Activity to plan for the release"
+question6options = "a) Activity to Introspect and Adapt \n b) Activity to improve Scrum Processes \n c) Activity to seek approval for the work done \n d) Activity to plan for the next Sprint \n e) Activity to plan for the release"
 question6answer = "A"
 questions.append((question6, question6options, question6answer))
 
 question7 = "Who is responsible to measure the Project’s performance?"
-question7options = "a) The Scrum Master /n b) The Delivery Manager /n c) The Product Owner /n d) The Development Team /n e) The Scrum Team"
+question7options = "a) The Scrum Master \n b) The Delivery Manager \n c) The Product Owner \n d) The Development Team \n e) The Scrum Team"
 question7answer = "C"
 questions.append((question7, question7options, question7answer))
 
 question8 = "What do we mean by a cross-functional Development Team?"
-question8options = "a) Each of the Development Team members should be cross-functional /n b) The developer should able to create test cases and execute them /n c) The Development Team should collaborate with the other Development Teams /n d) The Development Team consist of Developers and Testers /n e) The Development Team should have all the skills necessary to deliver the Done Increment"
+question8options = "a) Each of the Development Team members should be cross-functional \n b) The developer should able to create test cases and execute them \n c) The Development Team should collaborate with the other Development Teams \n d) The Development Team consist of Developers and Testers \n e) The Development Team should have all the skills necessary to deliver the Done Increment"
 question8answer = "E"
 questions.append((question8, question8options, question8answer))
 
 question9 = "What does a BurnDown Chart display?"
-question9options = "a) Project Progress /n b) Amount of remaining work with respect to time /n c) The velocity of the team /n d) The capacity of the team members /n e) How many more items can be picked up in a Sprint"
+question9options = "a) Project Progress \n b) Amount of remaining work with respect to time \n c) The velocity of the team \n d) The capacity of the team members \n e) How many more items can be picked up in a Sprint"
 question9answer = "E"
 questions.append((question9, question9options, question9answer))
 
 question10 = " In Scrum, when is a Sprint Over?"
-question10options = "a) When all the Sprint Backlog Items are completed /n b) When the Product Owner suggests /n c) When all the Sprint Backlog tasks are completed /n d) When the final testing is completed /n e) When the time box expires"
+question10options = "a) When all the Sprint Backlog Items are completed \n b) When the Product Owner suggests \n c) When all the Sprint Backlog tasks are completed \n d) When the final testing is completed \n e) When the time box expires"
 question10answer = "E"
 questions.append((question10, question10options, question10answer))
 
 question11 = "Which of the following activity is not timeboxed?"
-question11options = "a) Sprint Retrospective /n b) Sprint /n c) Product Backlog Refinement /n d) Daily Scrum /n e) Sprint Review"
+question11options = "a) Sprint Retrospective n b) Sprint \n c) Product Backlog Refinement \n d) Daily Scrum \n e) Sprint Review"
 question11answer = "C"
 questions.append((question11, question11options, question11answer))
--- scrumGame.py
+++ scrumGame.py
@@ -1,100 +1,86 @@
-# player1score=0
-# player2score=0
-# player3score=0
+# import questions
 
-# question1="Which of the following is delivered at the end of a sprint?"
-# question1options="a) A document containing test cases for the current sprint/n b) An architectural design of the solution /n c) An increment of Done software /n d) Wireframes designs for User Interface /n e) A signed statement of work"
-# question1answer="C"
+questions = []
 
-# question2="Product Backlog should be ordered on the basis of?"
-# question2options="a) Value of the items being delivered /n b) The complexity of the items being delivered /n c) Size of the items being delivered /n d) The risk associated with the items /n e) Based on the Scrum Team choice"
-# question2answer= "A"
+question1="Which of the following is delivered at the end of a sprint?"
+question1options="""a) A document containing test cases for the current sprint 
+b) An architectural design of the solution 
+c) An increment of Done software 
+d) Wireframes designs for User Interface 
+e) A signed statement of work"""
+question1answer="C"
 
-# question3 = "In an Agile environment, what is the main responsibility of a tester?"
-# question3options = "a) Create test scenarios and test cases /n b) Finding bugs /n c) Create automation scripts /n d) Send test execution reports to the stakeholders /n e) There is no role as a Tester in Scrum"
-# question3answer = "E"
+questions.append((question1, question1options, question1answer))
 
-# question4 = "When is a Sprint Retrospective ceremony performed?"
-# question4options = "a) Whenever the team suggests /n b) At the end of each Sprint /n c) Whenever needed /n d) Whenever the Product Owner suggests /n e) Whenever the Scrum Master suggests"
-# question4answer = "B"
+question2="Product Backlog should be ordered on the basis of?"
+question2options="a) Value of the items being delivered \n b) The complexity of the items being delivered \n c) Size of the items being delivered \n d) The risk associated with the items \n e) Based on the Scrum Team choice"
+question2answer= "A"
 
-# question5 = "When can a Sprint be canceled?"
-# question5options = "a) The Sprint items are no longer needed /n b) Sprint can never be canceled /n c) When Development is unable to complete the work /n d) Information required to start the development is not available /n e) Whenever the Product Owner says"
-# question5answer = "E"
+questions.append((question2, question2options, question2answer))
 
-# question6 = "What is a Sprint Review?"
-# question6options = "a) Activity to Introspect and Adapt /n b) Activity to improve Scrum Processes /n c) Activity to seek approval for the work done /n d) Activity to plan for the next Sprint /n e) Activity to plan for the release"
-# question6answer = "A"
+question3 = "In an Agile environment, what is the main responsibility of a tester?"
+question3options = "a) Create test scenarios and test cases \n b) Finding bugs \n c) Create automation scripts \n d) Send test execution reports to the stakeholders \n e) There is no role as a Tester in Scrum"
+question3answer = "E"
+questions.append((question3, question3options, question3answer))
 
-# question7 = "Who is responsible to measure the Project’s performance?"
-# question7options = "a) The Scrum Master /n b) The Delivery Manager /n c) The Product Owner /n d) The Development Team /n e) The Scrum Team"
-# question7answer = "C"
+question4 = "When is a Sprint Retrospective ceremony performed?"
+question4options = "a) Whenever the team suggests \n b) At the end of each Sprint \n c) Whenever needed \n d) Whenever the Product Owner suggests \n e) Whenever the Scrum Master suggests"
+question4answer = "B"
+questions.append((question4, question4options, question4answer))
 
-# question8 = "What do we mean by a cross-functional Development Team?"
-# question8options = "a) Each of the Development Team members should be cross-functional /n b) The developer should able to create test cases and execute them /n c) The Development Team should collaborate with the other Development Teams /n d) The Development Team consist of Developers and Testers /n e) The Development Team should have all the skills necessary to deliver the Done Increment"
-# question8answer = "E"
+question5 = "When can a Sprint be canceled?"
+question5options = "a) The Sprint items are no longer needed \n b) Sprint can never be canceled \n c) When Development is unable to complete the work \n d) Information required to start the development is not available \n e) Whenever the Product Owner says"
+question5answer = "E"
+questions.append((question5, question5options, question5answer))
 
-# question9 = "What does a BurnDown Chart display?"
-# question9options = "a) Project Progress /n b) Amount of remaining work with respect to time /n c) The velocity of the team /n d) The capacity of the team members /n e) How many more items can be picked up in a Sprint"
-# question9answer = "E"
+question6 = "What is a Sprint Review?"
+question6options = "a) Activity to Introspect and Adapt \n b) Activity to improve Scrum Processes \n c) Activity to seek approval for the work done \n d) Activity to plan for the next Sprint \n e) Activity to plan for the release"
+question6answer = "A"
+questions.append((question6, question6options, question6answer))
 
-# question10 = " In Scrum, when is a Sprint Over?"
-# question10options = "a) When all the Sprint Backlog Items are completed /n b) When the Product Owner suggests /n c) When all the Sprint Backlog tasks are completed /n d) When the final testing is completed /n e) When the time box expires"
-# question10answer = "E"
+question7 = "Who is responsible to measure the Project’s performance?"
+question7options = "a) The Scrum Master \n b) The Delivery Manager \n c) The Product Owner \n d) The Development Team \n e) The Scrum Team"
+question7answer = "C"
+questions.append((question7, question7options, question7answer))
 
-# question11 = "Which of the following activity is not timeboxed?"
-# question11options = "a) Sprint Retrospective /n b) Sprint /n c) Product Backlog Refinement /n d) Daily Scrum /n e) Sprint Review"
-# question11answer = "C"
+question8 = "What do we mean by a cross-functional Development Team?"
+question8options = "a) Each of the Development Team members should be cross-functional \n b) The developer should able to create test cases and execute them \n c) The Development Team should collaborate with the other Development Teams \n d) The Development Team consist of Developers and Testers \n e) The Development Team should have all the skills necessary to deliver the Done Increment"
+question8answer = "E"
+questions.append((question8, question8options, question8answer))
 
-# #Here is how you declare a variable in python
-# intVariable = 43
-# stringVariable = ";laksjd;flkjas;ldkjf;ajsd;lkfj;laksdjf;lkja;slk"
+question9 = "What does a BurnDown Chart display?"
+question9options = "a) Project Progress \n b) Amount of remaining work with respect to time \n c) The velocity of the team \n d) The capacity of the team members \n e) How many more items can be picked up in a Sprint"
+question9answer = "E"
+questions.append((question9, question9options, question9answer))
 
-# #here is how you type cast in python
-# str(intVariable) 
-# int(intVariable)
+question10 = " In Scrum, when is a Sprint Over?"
+question10options = "a) When all the Sprint Backlog Items are completed \n b) When the Product Owner suggests \n c) When all the Sprint Backlog tasks are completed \n d) When the final testing is completed \n e) When the time box expires"
+question10answer = "E"
+questions.append((question10, question10options, question10answer))
 
-# #Here is how you get input from keyboard in python:
-# variableNamedReallyWell = input()
+question11 = "Which of the following activity is not timeboxed?"
+question11options = "a) Sprint Retrospective n b) Sprint \n c) Product Backlog Refinement \n d) Daily Scrum \n e) Sprint Review"
+question11answer = "C"
+questions.append((question11, question11options, question11answer))
 
-# #Here is how you prompt to get input from keyboard in python:
-# variableNamedReallyWell = input("This is the string that will be printed to console immediately before the compiler waits for input from user.")
-
-# #Here is how you do if statements in python:
-# if(arbitraryCondition):
-#     #note the indent. The indent is vital for python
-#     print("Here is how you print stuff without expecting any input")
-# elif(otherCondition):
-#     #python has a shorthand for else-if statements which is "elif"
-#     print("This will print to an output.")
-# else:
-#     print("Every print statement is automatically concluded with a newline. ")
-
-# rowNumber = 0
-# #Here is how you do for loops in python
-# #this for-loop iterates through all of the indicies in table
-# for rows in table:
-#     print(rows + rowNumber)
-#     rowNumber=+1
-
-# #this for loop goes from 0 to 10. The range function generates an array from zero to 10
-# for i in range(0,10):
-#     print()
-#     #do something 10+1 times
 
 WantToQuit=""
-while WantToQuit !="q":
-    print("Welcome to Attack Jeparody. All the questions are based on scrum")
-    print("You are player 1")
-    print("Please select whether you would like to remove points from someone else, /n or add points to yourself")
-    action = input("Select 'b' to benefit yourself or 'a' to attack someone else")
-    print(action)
-    wager = input("Please make a wager")
-    print(wager)
+
+# questions = 
+print(questions[0])
+
+# while WantToQuit !="q":
+#     print("Welcome to Attack Jeparody. All the questions are based on scrum")
+#     print("You are player 1")
+#     print("Please select whether you would like to remove points from someone else, n or add points to yourself")
+#     action = input("Select 'b' to benefit yourself or 'a' to attack someone else")
+#     print(action)
+#     wager = input("Please make a wager")
+#     print(wager)
 
 
-    WantToQuit = input("Press enter to go onto next question or press 'q' to quit.")
-print("You have left the game-loop")
+#     WantToQuit = input("Press enter to go onto next question or press 'q' to quit.")
+# print("You have left the game-loop")
 
 
 
